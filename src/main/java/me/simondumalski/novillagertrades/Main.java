3ba8e735e99fa1bf6ede157d8d0c83dcf722e8d4
simondumalski <simon@dumalski.com>
2022-07-10 01:12:19 -0600

    Added the plugin onEnable logic

--- src/main/java/me/simondumalski/novillagertrades/Main.java
+++ src/main/java/me/simondumalski/novillagertrades/Main.java
@@ -1,5 +1,7 @@
 package me.simondumalski.novillagertrades;
 
+import me.simondumalski.novillagertrades.commands.NoVillagerTradesCommand;
+import me.simondumalski.novillagertrades.listeners.VillagerInteractListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -7,14 +9,16 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
 
+        //Load the config.yml
+        saveDefaultConfig();
+        reloadConfig();
+
+        //Register the event listeners
+        getServer().getPluginManager().registerEvents(new VillagerInteractListener(this), this);
+
+        //Set the command executors
+        getCommand("novillagertrades").setExecutor(new NoVillagerTradesCommand(this));
 
     }
 
-    @Override
-    public void onDisable() {
-
-
-
-    }
-    
 }
